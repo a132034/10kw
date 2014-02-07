f77a3d5b5261a0427979db59928984b4c86ab688
parkdoyoung <a132034@naver.com>
2014-02-07 20:31:56 +0900

    third - kyungme src complete

--- gen/com/masterpiecesoft/tenkw/R.java
+++ gen/com/masterpiecesoft/tenkw/R.java
@@ -50,39 +50,50 @@ public final class R {
         public static final int tapbar_4_o=0x7f02001a;
     }
     public static final class id {
-        public static final int Frame_Layout=0x7f080005;
-        public static final int GroupBtn=0x7f080013;
-        public static final int Group_ImgBt=0x7f080006;
-        public static final int Group_Km_txt=0x7f08000b;
-        public static final int Group_Name_txt=0x7f080008;
-        public static final int Group_PeopleNo_txt=0x7f080009;
-        public static final int Group_Percent_txt=0x7f08000a;
-        public static final int Group_Progress_bar=0x7f08000c;
-        public static final int Group_info=0x7f080007;
-        public static final int Main_Layout=0x7f080004;
-        public static final int Running_Start_Btn=0x7f08000e;
-        public static final int Running_Start_Layout=0x7f08000d;
-        public static final int action_settings=0x7f080015;
+        public static final int Frame_Layout=0x7f08000d;
+        public static final int GroupBtn=0x7f08001c;
+        public static final int Group_ImgBt=0x7f08000e;
+        public static final int Group_Km_txt=0x7f080013;
+        public static final int Group_Name_txt=0x7f080010;
+        public static final int Group_PeopleNo_txt=0x7f080011;
+        public static final int Group_Percent_txt=0x7f080012;
+        public static final int Group_Progress_bar=0x7f080014;
+        public static final int Group_info=0x7f08000f;
+        public static final int Main_Layout=0x7f08000c;
+        public static final int Running_Start_Btn=0x7f080016;
+        public static final int Running_Start_Layout=0x7f080015;
+        public static final int action_create=0x7f08001e;
         public static final int added_txt=0x7f080000;
         public static final int calendar_txt=0x7f080001;
-        public static final int detail_txt=0x7f080010;
-        public static final int graph_txt=0x7f080002;
-        public static final int group_pager=0x7f080003;
-        public static final int imageView1=0x7f080012;
-        public static final int personal_relative_layout=0x7f08000f;
-        public static final int section_label=0x7f080014;
-        public static final int textView1=0x7f080011;
+        public static final int create_item_checkbox=0x7f080009;
+        public static final int create_item_image=0x7f080006;
+        public static final int create_item_name_txt=0x7f080008;
+        public static final int create_item_phone_txt=0x7f080007;
+        public static final int create_left_btn=0x7f080004;
+        public static final int create_listView=0x7f080003;
+        public static final int create_right_btn=0x7f080005;
+        public static final int detail_txt=0x7f080019;
+        public static final int graph_txt=0x7f08000a;
+        public static final int group_pager=0x7f08000b;
+        public static final int imageView1=0x7f08001b;
+        public static final int main_personal_listView=0x7f080017;
+        public static final int member_search_txt=0x7f080002;
+        public static final int personal_relative_layout=0x7f080018;
+        public static final int section_label=0x7f08001d;
+        public static final int textView1=0x7f08001a;
     }
     public static final class layout {
         public static final int activity_added_friends=0x7f030000;
         public static final int activity_calendar=0x7f030001;
-        public static final int activity_graph=0x7f030002;
-        public static final int activity_group=0x7f030003;
-        public static final int activity_loading=0x7f030004;
-        public static final int activity_main=0x7f030005;
-        public static final int activity_personal_info=0x7f030006;
-        public static final int maindummy_fragment=0x7f030007;
-        public static final int tabdummy_fragment=0x7f030008;
+        public static final int activity_create_group=0x7f030002;
+        public static final int activity_creategroup_member=0x7f030003;
+        public static final int activity_graph=0x7f030004;
+        public static final int activity_group=0x7f030005;
+        public static final int activity_loading=0x7f030006;
+        public static final int activity_main=0x7f030007;
+        public static final int activity_personal_info=0x7f030008;
+        public static final int maindummy_fragment=0x7f030009;
+        public static final int tabdummy_fragment=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -90,6 +101,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int create_group=0x7f050007;
+        public static final int drawer_close=0x7f050006;
+        public static final int drawer_open=0x7f050005;
+        public static final int title_activity_create_group=0x7f050004;
         public static final int title_activity_group=0x7f050003;
         public static final int title_activity_loading=0x7f050002;
     }
--- src/com/masterpiecesoft/tenkw/layout/MainActivity.java
+++ src/com/masterpiecesoft/tenkw/layout/MainActivity.java
@@ -1,29 +1,33 @@
 package com.masterpiecesoft.tenkw.layout;
 
 import com.masterpiecesoft.tenkw.etc.*;
-
 import com.masterpiecesoft.tenkw.R;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends FragmentActivity {
  
 	private ImageButton StartBtn; // start btn 
 	private boolean mIsRunning;   // Running flag
-	private ListView personal;    // ?  
+	private ListView personal;    // 사이드바 예정 
 	
-	private DrawerLayout mDrawerLayout; //?
-	private ActionBarDrawerToggle mDrawerToggle; //?
+	private DrawerLayout mDrawerLayout; //사이드바 예정
+	private ActionBarDrawerToggle mDrawerToggle; //사이드바 예정
 	
 	private MainPagerAdapter mSectionsPagerAdapter;
 	private ViewPager mViewPager; 
@@ -39,8 +43,53 @@ public class MainActivity extends Activity {
 		// mainActivity가 실행되면 startActivity를 통해 LoadingActivity가 시작됨
 		startActivity(new Intent(this, LoadingActivity.class));
 		
+		// activity 와 layout.xml 을 연결시켜 준다. 
 		setContentView(R.layout.activity_main);
-		final ActionBar actionBar = getActionBar();
+		final ActionBar actionBar = getActionBar();// actionBar 를 가져옴 
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD); //actionBar 3 가지 모드중 기본 ( title 만 보이는 것) 선택
+
+		mSectionsPagerAdapter = new MainPagerAdapter(getSupportFragmentManager()); // MainImage Swipe 를 돕는 Adapter
+		
+		mViewPager = (ViewPager)findViewById(R.id.Group_ImgBt);// layout 의 swipe 위치(pager 로 범위 지정)를 java 에서 다룰수 있게?연결
+		mViewPager.setAdapter(mSectionsPagerAdapter); //pager 에 adapter 연결 
+		
+		StartBtn = (ImageButton)findViewById(R.id.Running_Start_Btn);
+		StartBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(mIsRunning){ // if start -> stop % stop -> start
+					mIsRunning = false;
+				}else{
+					mIsRunning = true;
+					Toast.makeText(getApplicationContext(), "start~", Toast.LENGTH_SHORT).show(); //for debug
+					//startService(new Intent(CopyOfMainActivity.this,StepService.class)); - 아마 만보기? 
+				}
+				
+			}
+		});
+	
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.Main_Layout);
+		// drawer layout 은  slide menu 를 보다 쉽게 이용하기 위해서 사용 근데 다시 애니메이션으로 바꿀까 고민중
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.btn_addgroup, R.string.drawer_open, R.string.drawer_close){
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				
+				super.onDrawerClosed(drawerView);
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				
+				super.onDrawerOpened(drawerView);
+			}
+			
+		};
+		// 토글은 액션바 왼쪽에 현재 < 나와있는 것, < 모양을 다른 이미지로 바꾸고 그 버튼을 통해 drawer layout 제어하려 했으나 실패 
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	
 	}
 
 	@Override
@@ -48,6 +97,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}// 메뉴 버튼 눌렀을 때 나오는게 메뉴 인줄 알았는데 액션바에서는 메뉴를 액션바 오른쪽에 나타내는 것 같음 따라서 xml 에 item 추가하여 + 버튼 추가 
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) // xml 로 정의한 버튼을 제어하는 곳  case 를 사용하여 누른 item 에 대한 일을 설정 , startactivity 는 activity 이동 앞에 loading 과 마찬가지
+		{
+		case R.id.action_create:
+			startActivity(new Intent(this, CreateGroupActivity.class));
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
